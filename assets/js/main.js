f80d7d747a8b9062b7bdf4b1003defa88dffa5b6
Dazai13 <dazai1317@mail.ru>
2025-06-01 16:28:00 +0700

    Готовый блок кредит аккордион баннер шапка

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/*Slider*/
 $(document).ready(function(){
   $('.banner__slider').slick({
     arrows:false,
@@ -9,4 +10,52 @@ $(document).ready(function(){
     centerMode: false,
     adaptiveHeight: true
   });
+});
+/*Accordion*/
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+    
+    accordionItems.forEach(item => {
+        const header = item.querySelector('.accordion__item-header');
+        const icon = header.querySelector('.accordion__header-icon use');
+        const content = item.querySelector('.accordion__item-content');
+        
+        header.addEventListener('click', () => {
+            accordionItems.forEach(otherItem => {
+                if (otherItem !== item && otherItem.classList.contains('active')) {
+                    otherItem.classList.remove('active');
+                    const otherIcon = otherItem.querySelector('.accordion__header-icon use');
+                    otherIcon.setAttribute('xlink:href', 'assets/images/sprite.svg#arrow');
+                    const otherContent = otherItem.querySelector('.accordion__item-content');
+                    otherContent.style.maxHeight = null;
+                }
+            });
+            
+            const isActive = item.classList.toggle('active');
+            
+            if (isActive) {
+                icon.setAttribute('xlink:href', 'assets/images/sprite.svg#line');
+                content.style.maxHeight = content.scrollHeight + 'px';
+                setTimeout(() => {
+                    content.style.maxHeight = 'none';
+                }, 300);
+            } else {
+                icon.setAttribute('xlink:href', 'assets/images/sprite.svg#arrow');
+                content.style.maxHeight = content.scrollHeight + 'px';
+                setTimeout(() => {
+                    content.style.maxHeight = '0';
+                }, 10);
+            }
+        });
+        
+        content.addEventListener('transitionend', function() {
+            if (!item.classList.contains('active')) {
+                content.style.maxHeight = null;
+            }
+        });
+        
+        if (item.classList.contains('active')) {
+            content.style.maxHeight = 'none';
+        }
+    });
 });
